f1dfc00f565f07e488a55acf21345483f9e38915
dennis090503 <dennislalwani09@gmail.com>
2025-04-24 23:43:21 +0530

    Initial commit

--- C/D1CPP.cpp
+++ C/D1CPP.cpp
@@ -15,4 +15,72 @@ using namespace std;
 
 //2
 
+//class MyClass{
+//	public:
+//		
+//		MyClass(){
+//			cout<<"Welcome\n";
+//		}
+//		
+//		~MyClass(){
+//			cout<<"Thankyou";
+//		}
+//		
+//		void triangle(){
+//			int i;
+//			for (i=0;i<=5;i++){
+//				cout<<"hello";
+//				cout<<"\n";
+//			}
+//		}
+//};
+//main(){
+//	
+//	MyClass obj;
+//	obj.triangle();
+//}
 
+
+
+
+class MyClass{
+	public:
+		
+		MyClass(){
+			cout<<"Welcome to hotel\n";
+		}
+		
+		~MyClass(){
+			cout<<"Thankyou visit again";
+		}
+		
+		void menu(){
+			int choice,sum=0;
+			while(1){
+				cout<<"1.dosa\n2.idli\n3.utaapa\n4.Exit\n";
+				cout<<"ENTER CHOICE:";
+				cin>>choice;
+				if (choice==1){
+					sum+=100;
+				}
+				else if(choice==2){
+					sum+=50;
+				}
+				else if(choice==3){
+					sum+=90;
+				}
+				else if(choice==4){
+					break;
+				}
+				else {
+					cout<<"enter valid choice\n";	
+				}
+			}
+			cout<<"Total:"<<sum<<"\n";	
+		}
+};
+main(){
+	
+	MyClass obj;
+	obj.menu();
+}
